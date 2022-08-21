8611ddd09254a7db472bf71dd0b47fe33666fe37
Pratul Pant <pratulpant02@gmail.com>
2022-08-21 23:44:43 +0530

    update 2.9

--- lib/view/account/your_orders_screen.dart
+++ lib/view/account/your_orders_screen.dart
@@ -1,6 +1,7 @@
 import 'package:burgerhub/constants/constant.dart';
 import 'package:burgerhub/models/order_model.dart';
 import 'package:burgerhub/widgets/AppBar/simple_appbar_widget.dart';
+import 'package:burgerhub/widgets/food%20widgets/food_type_widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -16,7 +17,7 @@ class YourOrderScreen extends StatelessWidget {
       appBar: SimpleAppBarWidget(
         hasBackButton: true,
       ),
-      body: SafeArea(
+      body: SingleChildScrollView(
         child: Column(
           children: [
             Center(
@@ -46,6 +47,8 @@ class YourOrderScreen extends StatelessWidget {
                     itemCount: snapshot.data!.docs.length,
                     shrinkWrap: true,
                     itemBuilder: (context, index) {
+                      String orderStatus =
+                          snapshot.data!.docs[index].data()['orderStatus'];
                       return Container(
                         margin:
                             EdgeInsets.symmetric(horizontal: 10, vertical: 10),
@@ -55,40 +58,133 @@ class YourOrderScreen extends StatelessWidget {
                           ),
                           borderRadius: BorderRadius.circular(10),
                         ),
-                        child: ExpansionTile(
-                          textColor: primaryColor,
-                          title: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    'Order No. ${index}',
-                                    style: productTitleStyle,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                  SizedBox(
-                                    height: screenSize.height * .02,
-                                  ),
-                                  Text(
-                                    DateFormat('dd-MM-yyyy').format(snapshot
-                                        .data!.docs[index]
-                                        .data()['orderedAt']
-                                        .toDate()),
-                                    style: TextStyle(
-                                      fontSize: 13,
-                                      color: Colors.grey.shade600,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: ExpansionTile(
+                            collapsedBackgroundColor: Colors.white70,
+                            textColor: primaryColor,
+                            title: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Order No. ${index}',
+                                      style: productTitleStyle,
+                                      overflow: TextOverflow.ellipsis,
                                     ),
-                                  )
-                                ],
-                              ),
+                                    SizedBox(
+                                      height: screenSize.height * .02,
+                                    ),
+                                    Text(
+                                      orderStatus,
+                                      style: secondaryTitleStyle.copyWith(
+                                        color: orderStatus == "Preparing" ||
+                                                orderStatus == 'Completed'
+                                            ? Colors.green
+                                            : Colors.red,
+                                        fontSize: 13,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                Container(
+                                  child: CircleAvatar(
+                                    radius: screenSize.height * .03,
+                                    backgroundColor: Colors.transparent,
+                                    backgroundImage:
+                                        AssetImage('assets/myorder.png'),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            children: [
                               Container(
-                                child: CircleAvatar(
-                                  radius: screenSize.height * .03,
-                                  backgroundColor: Colors.transparent,
-                                  backgroundImage:
-                                      AssetImage('assets/myorder.png'),
+                                margin: EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 10),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Divider(
+                                      color: Colors.grey,
+                                    ),
+                                    StreamBuilder(
+                                        stream: FirebaseFirestore.instance
+                                            .collection('orders')
+                                            .doc(snapshot.data!.docs[index]
+                                                .data()['orderNumber'])
+                                            .collection('products')
+                                            .snapshots(),
+                                        builder: (context,
+                                            AsyncSnapshot<
+                                                    QuerySnapshot<
+                                                        Map<String, dynamic>>>
+                                                snapshot1) {
+                                          if (snapshot1.connectionState ==
+                                              ConnectionState.waiting) {
+                                            return Center(
+                                              child:
+                                                  CircularProgressIndicator(),
+                                            );
+                                          }
+                                          return ListView.builder(
+                                              physics: ScrollPhysics(),
+                                              itemCount:
+                                                  snapshot1.data!.docs.length,
+                                              shrinkWrap: true,
+                                              itemBuilder: (context, index1) {
+                                                return Row(
+                                                  children: [
+                                                    foodTypeWidget(
+                                                        foodtype: snapshot1
+                                                            .data!.docs[index1]
+                                                            .data()['type']),
+                                                    Text(
+                                                      '${snapshot1.data!.docs[index1].data()['quantity']} ',
+                                                      style: TextStyle(
+                                                          color: Colors
+                                                              .grey.shade600),
+                                                    ),
+                                                    Text(
+                                                      'X',
+                                                      style: TextStyle(
+                                                          color: Colors
+                                                              .grey.shade600),
+                                                    ),
+                                                    Text(
+                                                      ' ${snapshot1.data!.docs[index1].data()['productName']} ',
+                                                      style: TextStyle(
+                                                          color: Colors.black),
+                                                    ),
+                                                  ],
+                                                );
+                                              });
+                                        }),
+                                    Divider(
+                                      color: Colors.grey,
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          DateFormat('dd-MM-yyyy').format(
+                                              snapshot.data!.docs[index]
+                                                  .data()['orderedAt']
+                                                  .toDate()),
+                                          style: labelTitleStyle.copyWith(
+                                              color: Colors.grey),
+                                        ),
+                                        Text(
+                                          '₹${snapshot.data!.docs[index].data()['totalPrice']}',
+                                          style: productPricingStyle.copyWith(
+                                              color: Colors.black),
+                                        ),
+                                      ],
+                                    )
+                                  ],
                                 ),
                               ),
                             ],
